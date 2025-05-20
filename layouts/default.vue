643acedabf1395ea7f9bd28658bbe22e5c60ec60
dp <dp@gmail.com>
2025-05-20 22:46:14 +0200

    add layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex min-h-screen flex-col">
+    <AppNavBar />
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
