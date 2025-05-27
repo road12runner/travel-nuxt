a54568f6d9b67ddcec927e052d77eadc297609a3
dp <dp@gmail.com>
2025-05-27 22:08:54 +0200

    add env parse

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,7 @@ export default withNuxt(antfu({
     quotes: "double",
   },
   rules: {
+    "ts/no-redeclare": "off",
     "ts/consistent-type-definitions": ["error", "type"],
     "no-console": ["warn"],
     "antfu/no-top-level-await": "off",
